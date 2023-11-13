de46c4079c31b645c0fce8a7b2fb5f95898cea08
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 11:54:25 +0530

    Update auto_rename.py

--- plugins/auto_rename.py
+++ plugins/auto_rename.py
@@ -1,5 +1,9 @@
 import re
-from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.enums import MessageMediaType
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
 
 def extract_info_from_file(file_name):
     pattern = r'\[(.*?)\]\s*(.*?)\s*[Ss](\d+)\s*-\s*[Ee](\d+)\s*(\d+p)?\s*\[([^\]]+)\]\s*@(.*)\.mkv'
