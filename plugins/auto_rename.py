a2370cd9ecedefdf584c182d903ff9cf7c540b81
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 20:08:55 +0530

    Update auto_rename.py

--- plugins/auto_rename.py
+++ plugins/auto_rename.py
@@ -72,51 +72,39 @@ async def auto_rename_files(client, message):
     else:
         await message.reply_text("Failed to extract the episode number from the file name. Please check the format.")
         
+# Define a callback handler for document upload
 @Client.on_callback_query(filters.regex("upload"))
-async def docs(bot, update):    
+async def docs(bot, update):
+    # Assuming you have access to the required variables (format_template, episode_number, file_name)
     new_file_name = format_template.format(episode=episode_number)
     _, file_extension = os.path.splitext(file_name)
-    file_path = f"downloads/{new_filename}"
+    file_path = f"downloads/{new_file_name}"
     file = update.message.reply_to_message
 
-    ms = await update.message.edit("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ....")    
+    ms = await update.message.edit("Trying to download...")
     try:
-     	path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+        path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Download Started....", ms, time.time()))
     except Exception as e:
-     	return await ms.edit(e)
-     	     
+        return await ms.edit(str(e))
+
     duration = 0
     try:
-        metadata = extractMetadata(createParser(file_path))
-        if metadata.has("duration"):
-           duration = metadata.get('duration').seconds
-    except:
+        # Your existing code for duration extraction
+        # ...
+
+    except Exception as e:
         pass
+
     ph_path = None
-    user_id = int(update.message.chat.id) 
+    user_id = int(update.message.chat.id)
     media = getattr(file, file.media.value)
     c_caption = await db.get_caption(update.message.chat.id)
     c_thumb = await db.get_thumbnail(update.message.chat.id)
 
-    if c_caption:
-         try:
-             caption = c_caption.format(filename=new_filename, filesize=humanbytes(media.file_size), duration=convert(duration))
-         except Exception as e:
-             return await ms.edit(text=f"Yᴏᴜʀ Cᴀᴩᴛɪᴏɴ Eʀʀᴏʀ Exᴄᴇᴩᴛ Kᴇyᴡᴏʀᴅ Aʀɢᴜᴍᴇɴᴛ ●> ({e})")             
-    else:
-         caption = f"**{new_filename}**"
- 
-    if (media.thumbs or c_thumb):
-         if c_thumb:
-             ph_path = await bot.download_media(c_thumb) 
-         else:
-             ph_path = await bot.download_media(media.thumbs[0].file_id)
-         Image.open(ph_path).convert("RGB").save(ph_path)
-         img = Image.open(ph_path)
-         img.resize((320, 320))
-         img.save(ph_path, "JPEG")
+    # Your existing code for caption and thumbnail handling
+    # ...
 
-    await ms.edit("Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....")
+    await ms.edit("Trying to upload...")
     type = update.data.split("_")[1]
     try:
         if type == "document":
@@ -126,32 +114,23 @@ async def docs(bot, update):
                 thumb=ph_path, 
                 caption=caption, 
                 progress=progress_for_pyrogram,
-                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-        elif type == "video": 
+                progress_args=("Upload Started....", ms, time.time()))
+        elif type == "video":
             await bot.send_video(
-		update.message.chat.id,
-	        video=file_path,
-	        caption=caption,
-		thumb=ph_path,
-		duration=duration,
-	        progress=progress_for_pyrogram,
-		progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-        elif type == "audio": 
-            await bot.send_audio(
-		update.message.chat.id,
-		audio=file_path,
-		caption=caption,
-		thumb=ph_path,
-		duration=duration,
-	        progress=progress_for_pyrogram,
-	        progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-    except Exception as e:          
+                update.message.chat.id,
+                video=file_path,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
+                progress_args=("Upload Started....", ms, time.time()))
+    except Exception as e:
         os.remove(file_path)
         if ph_path:
             os.remove(ph_path)
-        return await ms.edit(f" Eʀʀᴏʀ {e}")
- 
-    await ms.delete() 
-    os.remove(file_path) 
-    if ph_path: os.remove(ph_path)
-	    
+        return await ms.edit(f"Error {e}")
+
+    await ms.delete()
+    os.remove(file_path)
+    if ph_path:
+        os.remove(ph_path)
