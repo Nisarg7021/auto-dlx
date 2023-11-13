2376f9b9658726e2e29b32247a94b3636bf3b02c
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 17:38:39 +0530

    Update auto_rename.py

--- plugins/auto_rename.py
+++ plugins/auto_rename.py
@@ -49,8 +49,6 @@ async def auto_rename_command(client, message):
 
     await message.reply_text("Auto rename format updated successfully!")
 
-
-# Inside the handler for file uploads
 @Client.on_message(filters.private & (filters.document | filters.video))
 async def auto_rename_files(client, message):
     user_id = message.from_user.id
@@ -63,21 +61,95 @@ async def auto_rename_files(client, message):
     file_name = message.document.file_name
     print(f"Original File Name: {file_name}")
 
-    extracted_info = extract_info_from_file(file_name)
-    print(f"Extracted Info: {extracted_info}")
+    episode_number = extract_episode_number(file_name)
+    print(f"Extracted Episode Number: {episode_number}")
 
-    if extracted_info:
-        # Use the extracted_info dictionary and format_template string to generate the new file name
-        new_file_name = format_template.format(
-            series=extracted_info["Series Title"],
-            season=extracted_info["Season Number"],
-            episode=extracted_info["Episode Number"],
-            quality=extracted_info["Quality"],
-            subtitle=extracted_info["Subtitle"],
-            source_group=extracted_info["Source/Group"],
-        )
-
+    if episode_number:
+        # Use the episode_number and format_template string to generate the new file name
+        new_file_name = format_template.format(episode=episode_number)
         await message.reply_text(f"File renamed successfully to: {new_file_name}")
     else:
-        await message.reply_text("Failed to extract information from the file name. Please check the format.")
+        await message.reply_text("Failed to extract the episode number from the file name. Please check the format.")
         
+@Client.on_callback_query(filters.regex("upload"))
+async def docs(bot, update):    
+    new_file_name = format_template.format(episode=episode_number)
+    _, file_extension = os.path.splitext(file_name)
+    file_path = f"downloads/{new_filename}"
+    file = update.message.reply_to_message
+
+    ms = await update.message.edit("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ....")    
+    try:
+     	path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+    except Exception as e:
+     	return await ms.edit(e)
+     	     
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
+    ph_path = None
+    user_id = int(update.message.chat.id) 
+    media = getattr(file, file.media.value)
+    c_caption = await db.get_caption(update.message.chat.id)
+    c_thumb = await db.get_thumbnail(update.message.chat.id)
+
+    if c_caption:
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanbytes(media.file_size), duration=convert(duration))
+         except Exception as e:
+             return await ms.edit(text=f"Yᴏᴜʀ Cᴀᴩᴛɪᴏɴ Eʀʀᴏʀ Exᴄᴇᴩᴛ Kᴇyᴡᴏʀᴅ Aʀɢᴜᴍᴇɴᴛ ●> ({e})")             
+    else:
+         caption = f"**{new_filename}**"
+ 
+    if (media.thumbs or c_thumb):
+         if c_thumb:
+             ph_path = await bot.download_media(c_thumb) 
+         else:
+             ph_path = await bot.download_media(media.thumbs[0].file_id)
+         Image.open(ph_path).convert("RGB").save(ph_path)
+         img = Image.open(ph_path)
+         img.resize((320, 320))
+         img.save(ph_path, "JPEG")
+
+    await ms.edit("Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....")
+    type = update.data.split("_")[1]
+    try:
+        if type == "document":
+            await bot.send_document(
+                update.message.chat.id,
+                document=file_path,
+                thumb=ph_path, 
+                caption=caption, 
+                progress=progress_for_pyrogram,
+                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+        elif type == "video": 
+            await bot.send_video(
+		update.message.chat.id,
+	        video=file_path,
+	        caption=caption,
+		thumb=ph_path,
+		duration=duration,
+	        progress=progress_for_pyrogram,
+		progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+        elif type == "audio": 
+            await bot.send_audio(
+		update.message.chat.id,
+		audio=file_path,
+		caption=caption,
+		thumb=ph_path,
+		duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
+    except Exception as e:          
+        os.remove(file_path)
+        if ph_path:
+            os.remove(ph_path)
+        return await ms.edit(f" Eʀʀᴏʀ {e}")
+ 
+    await ms.delete() 
+    os.remove(file_path) 
+    if ph_path: os.remove(ph_path) u
