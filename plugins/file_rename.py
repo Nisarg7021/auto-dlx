3eb5c4cb7d9069bbf47b66453e56c2d43122dfe4
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 10:59:21 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -14,37 +14,30 @@ from PIL import Image
 import os, time
 
 
-@Client.on_message(filters.private & filters.command("rename", prefixes="/") & filters.reply & (filters.document | filters.audio | filters.video))
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
 async def rename_start(client, message):
-    print("Command received and filters passed.")
-    
-    if message.media is None:
-        print("No media found in the replied-to message.")
-        return await message.reply_text("This command requires a reply to a media message.")
-    
     file = getattr(message, message.media.value)
     filename = file.file_name  
     if file.file_size > 2000 * 1024 * 1024:
-        print("File size is too big.")
-        return await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB")
+         return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
 
     try:
         await message.reply_text(
-            text=f"**__Please enter the new file name:__**\n\n**Old File Name:** `{filename}`",
-            reply_markup=ForceReply(True)
-        )
-        
-        # Allow more time for users to reply (e.g., 120 seconds)
-        await sleep(120)
+            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
+	    reply_to_message_id=message.id,  
+	    reply_markup=ForceReply(True)
+        )       
+        await sleep(30)
     except FloodWait as e:
         await sleep(e.value)
         await message.reply_text(
-            text=f"**__Please enter the new file name:__**\n\n**Old File Name:** `{filename}`",
-            reply_markup=ForceReply(True)
+            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
+	    reply_to_message_id=message.id,  
+	    reply_markup=ForceReply(True)
         )
     except:
         pass
-	    
+
 
 
 @Client.on_message(filters.private & filters.reply)
