667c142952fd26f2dfa3530ad633a679515cb6f3
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 10:00:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,9 +30,6 @@ class Config(object):
     API_HASH  = os.environ.get("API_HASH", "d8c8dab274f9a811814a6a96d044028e")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6985292832:AAHGVJPolS-BlP4fAHD4Zvun-IJJx0XF14E") 
 
-    # Auto Rename Status
-    AUTO_RENAME = False
-       
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://mehtadmphta33:Mehtab1234@cluster0.bfsb3oq.mongodb.net/?retryWrites=true&w=majority")
