04014ee753d1c8be81c9f36def9378a59b52f3cc
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 10:46:38 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -27,24 +27,20 @@ async def rename_start(client, message):
     if file.file_size > 2000 * 1024 * 1024:
         print("File size is too big.")
         return await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB")
-	    
+
     try:
         await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
-        )       
-        await sleep(30)
+            text=f"**__Please enter the new file name:__**\n\n**Old File Name:** `{filename}`",
+            reply_markup=ForceReply(True)
+        )
     except FloodWait as e:
         await sleep(e.value)
         await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
+            text=f"**__Please enter the new file name:__**\n\n**Old File Name:** `{filename}`",
+            reply_markup=ForceReply(True)
         )
     except:
-        pass
-
+        pass	    
 
 
 @Client.on_message(filters.private & filters.reply)
