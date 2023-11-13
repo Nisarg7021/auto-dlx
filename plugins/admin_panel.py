1b0e80ade411d0613b2d3677c202d2e3b364b730
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-13 08:53:10 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -21,6 +21,7 @@ Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
 License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
 """
 
+import re
 from config import Config
 from helper.database import db
 from pyrogram.types import Message
