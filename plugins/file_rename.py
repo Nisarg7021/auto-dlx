5b17d32348c70bdff50b4aa21c2a318fdc71e8c2
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-20 12:01:04 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -14,144 +14,125 @@ from PIL import Image
 import os, time
 
 
-@Client.on_message(filters.private & filters.command("rename", prefixes="/") & filters.reply & (filters.document | filters.audio | filters.video))
-async def rename_start(client, message):
-    # Check if the replied message is a media file
-    if message.media is None:
-        return await message.reply_text("This command requires a reply to a media message.")
+def extract_episode_number(filename):
+    # Pattern 1: S1E01 or S01E01
+    pattern1 = re.compile(r'S(\d+)E(\d+)')
+    
+    # Pattern 2: S02 E01
+    pattern2 = re.compile(r'S(\d+) E(\d+)')
+    
+    # Pattern 3: Episode Number After "E" or "-"
+    pattern3 = re.compile(r'[E|-](\d+)')
+    
+    # Pattern 4: Standalone Episode Number
+    pattern4 = re.compile(r'(\d+)')
+    
+    # Try each pattern in order
+    for pattern in [pattern1, pattern2, pattern3, pattern4]:
+        match = re.search(pattern, filename)
+        if match:
+            return match.group(1)  # Extracted episode number
+    
+    # Return None if no pattern matches
+    return None
 
-    # Get file details
-    file = getattr(message, message.media.value)
-    filename = file.file_name  
+# Example Usage:
+filename = "One Piece S1-07 [720p][Dual] @Anime_Edge.mkv"
+episode_number = extract_episode_number(filename)
+print(f"Extracted Episode Number: {episode_number}")
 
-    # Check file size
-    if file.file_size > 2000 * 1024 * 1024:
-        return await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB")
+# Assuming you have a command handler in Pyrogram
+@Client.on_message(filters.private & filters.command("autorename"))
+async def auto_rename_command(client, message):
+    user_id = message.from_user.id
 
-    try:
-        # Prompt the user to enter a new file name
-        await message.reply_text(
-            text=f"**__Please enter the new file name:__**\n\n**Old File Name:** `{filename}`",
-            reply_markup=ForceReply(True)
-        )
+    # Extract the format from the command
+    format_template = message.text.split("/autorename", 1)[1].strip()
 
-        # Allow more time for users to reply (e.g., 120 seconds)
-        await sleep(120)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Save the format template to the database
+    await db.set_format_template(user_id, format_template)
 
-# Define the /rename callback handler
-@Client.on_message(filters.private & filters.reply)
-async def refunc(client, message):
-    reply_message = message.reply_to_message
+    await message.reply_text("Auto rename format updated successfully!")
 
-    # Check if the replied message has a ForceReply
-    if reply_message.reply_markup and isinstance(reply_message.reply_markup, ForceReply):
-        new_name = message.text 
-        await message.delete() 
-        msg = await client.get_messages(message.chat.id, reply_message.id)
-        file = msg.reply_to_message
+# Inside the handler for file uploads
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
+async def auto_rename_files(client, message):
+    user_id = message.from_user.id
+    format_template = await db.get_format_template(user_id)
+
+    if not format_template:
+        return await message.reply_text("Please set an auto rename format first using /autorename")
+
+    # Extract information from the incoming file name
+    file_name = message.document.file_name
+    print(f"Original File Name: {file_name}")
+
+    episode_number = extract_episode_number(file_name)
+    print(f"Extracted Episode Number: {episode_number}")
+
+    if episode_number:
+        # Use the episode_number and format_template string to generate the new file name
+        new_file_name = format_template.format(episode=episode_number)
+        await message.reply_text(f"File renamed successfully to: {new_file_name}")
+
+        # Assuming you have access to the required variables (format_template, episode_number, file_name)
+        _, file_extension = os.path.splitext(file_name)
+        file_path = f"downloads/{new_file_name}"
+        file = message
+
+        ms = await message.reply("Trying to download...")
+        try:
+            path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Download Started....", ms, time.time()))
+        except Exception as e:
+            return await ms.edit(str(e))
+
+        duration = 0
+        try:
+            duration = get_duration(file_path)
+        except Exception as e:
+            print(f"Error getting duration: {e}")
+            duration = 0
+        finally:
+            pass
+
+        ph_path = None
+        user_id = int(message.chat.id)
         media = getattr(file, file.media.value)
-        if not "." in new_name:
-            if "." in media.file_name:
-                extn = media.file_name.rsplit('.', 1)[-1]
-            else:
-                extn = "mkv"
-            new_name = new_name + "." + extn
-        await reply_message.delete()
+        c_caption = await db.get_caption(message.chat.id)
+        c_thumb = await db.get_thumbnail(message.chat.id)
 
-        button = [[InlineKeyboardButton("📁 Dᴏᴄᴜᴍᴇɴᴛ",callback_data = "upload_document")]]
-        if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
-            button.append([InlineKeyboardButton("🎥 Vɪᴅᴇᴏ", callback_data = "upload_video")])
-        elif file.media == MessageMediaType.AUDIO:
-            button.append([InlineKeyboardButton("🎵 Aᴜᴅɪᴏ", callback_data = "upload_audio")])
-        await message.reply(
-            text=f"**Sᴇʟᴇᴄᴛ Tʜᴇ Oᴜᴛᴩᴜᴛ Fɪʟᴇ Tyᴩᴇ**\n**• Fɪʟᴇ Nᴀᴍᴇ :-**```{new_name}```",
-            reply_to_message_id=file.id,
-            reply_markup=InlineKeyboardMarkup(button)
-        )
+        # ... (your existing code for caption and thumbnail handling)
 
+        await ms.edit("Trying to upload...")
+        type = file.media.document.mime_type.split("/")[0].lower()
+        try:
+            if type == "document":
+                await bot.send_document(
+                    message.chat.id,
+                    document=file_path,
+                    thumb=ph_path, 
+                    caption=caption, 
+                    progress=progress_for_pyrogram,
+                    progress_args=("Upload Started....", ms, time.time()))
+            elif type in ["video", "audio"]:
+                await bot.send_video(
+                    message.chat.id,
+                    video=file_path,
+                    caption=caption,
+                    thumb=ph_path,
+                    duration=duration,
+                    progress=progress_for_pyrogram,
+                    progress_args=("Upload Started....", ms, time.time()))
+        except Exception as e:
+            os.remove(file_path)
+            if ph_path:
+                os.remove(ph_path)
+            return await ms.edit(f"Error {e}")
 
-
-@Client.on_callback_query(filters.regex("upload"))
-async def doc(bot, update):    
-    new_name = update.message.text
-    new_filename = new_name.split(":-")[1]
-    file_path = f"downloads/{new_filename}"
-    file = update.message.reply_to_message
-
-    ms = await update.message.edit("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ....")    
-    try:
-     	path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
-    except Exception as e:
-     	return await ms.edit(e)
-     	     
-    duration = 0
-    try:
-        metadata = extractMetadata(createParser(file_path))
-        if metadata.has("duration"):
-           duration = metadata.get('duration').seconds
-    except:
-        pass
-    ph_path = None
-    user_id = int(update.message.chat.id) 
-    media = getattr(file, file.media.value)
-    c_caption = await db.get_caption(update.message.chat.id)
-    c_thumb = await db.get_thumbnail(update.message.chat.id)
-
-    if c_caption:
-         try:
-             caption = c_caption.format(filename=new_filename, filesize=humanbytes(media.file_size), duration=convert(duration))
-         except Exception as e:
-             return await ms.edit(text=f"Yᴏᴜʀ Cᴀᴩᴛɪᴏɴ Eʀʀᴏʀ Exᴄᴇᴩᴛ Kᴇyᴡᴏʀᴅ Aʀɢᴜᴍᴇɴᴛ ●> ({e})")             
-    else:
-         caption = f"**{new_filename}**"
- 
-    if (media.thumbs or c_thumb):
-         if c_thumb:
-             ph_path = await bot.download_media(c_thumb) 
-         else:
-             ph_path = await bot.download_media(media.thumbs[0].file_id)
-         Image.open(ph_path).convert("RGB").save(ph_path)
-         img = Image.open(ph_path)
-         img.resize((320, 320))
-         img.save(ph_path, "JPEG")
-
-    await ms.edit("Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....")
-    type = update.data.split("_")[1]
-    try:
-        if type == "document":
-            await bot.send_document(
-                update.message.chat.id,
-                document=file_path,
-                thumb=ph_path, 
-                caption=caption, 
-                progress=progress_for_pyrogram,
-                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-        elif type == "video": 
-            await bot.send_video(
-		update.message.chat.id,
-	        video=file_path,
-	        caption=caption,
-		thumb=ph_path,
-		duration=duration,
-	        progress=progress_for_pyrogram,
-		progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-        elif type == "audio": 
-            await bot.send_audio(
-		update.message.chat.id,
-		audio=file_path,
-		caption=caption,
-		thumb=ph_path,
-		duration=duration,
-	        progress=progress_for_pyrogram,
-	        progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))
-    except Exception as e:          
+        await ms.delete()
         os.remove(file_path)
         if ph_path:
             os.remove(ph_path)
-        return await ms.edit(f" Eʀʀᴏʀ {e}")
- 
-    await ms.delete() 
-    os.remove(file_path) 
-    if ph_path: os.remove(ph_path) 
+    else:
+        await message.reply_text("Failed to extract the episode number from the file name. Please check the format.")
+	    
