8577e8861e4f1ee1f92a2ec0048c26f988a06e74
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-21 16:45:08 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -5,37 +5,55 @@ from config import Config, Txt
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def progress_for_pyrogram(current, total, ud_type, message, start, *args, **kwargs):
+async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:        
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
+    percent = f"{current * 100 / total:.1f}%"
+    speed = humanbytes((current / diff) * (total / 100))
+    elapsed_time = round(diff)
+    time_to_completion = round((total - current) / (current / diff))
 
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+    progress_str = "`{0}` | `{1}` | `{2}` | `{3}` | `{4}`".format(
+        percent, humanbytes(current), humanbytes(total), speed, elapsed_time
+    )
 
-        progress = "{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
-        )            
-        tmp = progress + Txt.PROGRESS_BAR.format( 
-            round(percentage, 2),
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),            
-            estimated_total_time if estimated_total_time != '' else "0 s"
+    try:
+        await message.edit_text(
+            text=(
+                "{0}\n\n"
+                "<b>Progress:</b> {1}\n"
+                "<b>ETA:</b> {2}".format(
+                    ud_type, progress_str, convert(time_to_completion)
+                )
+            ),
+            parse_mode="markdown",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Cancel",
+                            callback_data=f"cancel_upload_{message.message_id}",
+                        )
+                    ]
+                ]
+            ),
         )
-        try:
-            await message.edit(
-                text=f"{ud_type}\n\n{tmp}",               
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])                                               
-            )
-        except:
-            pass
+    except Exception as e:
+        print(f"Error in progress_for_pyrogram: {e}")
+
+# Usage:
+ms = await message.reply("Trying to download...")
+try:
+    path = await client.download_media(
+        message=file,
+        file_name=file_path,
+        progress=lambda current, total: progress_for_pyrogram(
+            current, total, "Download", ms, time.time()
+        ),
+    )
+except Exception as e:
+    return await ms.edit(str(e))
+    
             
 
 def humanbytes(size):    
