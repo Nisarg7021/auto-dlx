2cf43767ebb42643bdcf25ed1f9714236dd896da
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-01 04:59:25 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -14,8 +14,7 @@ import re
 import time
 
 
-@Client.on_message(filters.private & filters.command("autorename"))
-async def auto_rename_command(client,def extract_episode_and_quality(filename):
+def extract_episode_and_quality(filename):
     # Pattern 1: S1E01 or S01E01 with quality
     pattern1 = re.compile(r'S(\d+)E(\d+).*?(\d{3,4}p)')
 
