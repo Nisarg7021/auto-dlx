2c8bf520f2689f52a44771ef4250562fd7f025bf
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-11-30 07:08:38 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -36,7 +36,8 @@ def extract_episode_and_quality(filename):
     
     # Return None if no pattern matches
     return None, None
-
+    
+file_name = message.document.file_name
 filename = {file_name}
 episode_number, quality = extract_episode_and_quality(filename)
 print(f"Extracted Episode Number: {episode_number}")
