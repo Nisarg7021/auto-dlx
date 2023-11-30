925a1c686252cdd4a2e80f9397172833bd2d5df3
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-01 05:01:47 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -91,16 +91,16 @@ async def auto_rename_files(client, message):
 
     new_file_name = format_template.format(season=season_number, episode=episode_number, quality=quality)
     await message.reply_text(f"File renamed successfully to: {new_file_name}")
-        
-        _, file_extension = os.path.splitext(file_name)
-        file_path = f"downloads/{new_file_name}"
-        file = message
-        
-        ms = await message.reply("Trying to download...")
-        try:
-            path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
-        except Exception as e:
-            return await ms.edit(e)
+    
+    _, file_extension = os.path.splitext(file_name)
+    file_path = f"downloads/{new_file_name}"
+    file = message
+    
+    ms = await message.reply("Trying to download...")
+    try:
+        path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+    except Exception as e:
+        return await ms.edit(e)
 
         duration = 0
         try:
