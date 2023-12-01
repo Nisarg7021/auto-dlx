692f2cf75abe120ba77453e6bcfa8d115d1ab971
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-01 06:25:37 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -91,10 +91,9 @@ async def auto_rename_files(client, message):
     
     ms = await message.reply("Trying to download...")
     try:
-            path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
-        except Exception as e:
-            return await ms.edit(e)
-
+        path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", ms, time.time()))                    
+    except Exception as e :
+        return await ms.edit(e)
         duration = 0
         try:
             metadata = extractMetadata(createParser(file_path))
