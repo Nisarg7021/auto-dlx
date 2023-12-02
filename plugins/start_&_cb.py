f3772bfac5a2a682096df3142235eff69f9ece0e
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-03 04:54:41 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -124,10 +124,10 @@ async def cb_handler(client, query: CallbackQuery):
         )
       
     elif data == "thumbnail":
-    # Assuming you have a method in your database to get the user's thumbnail
-    user_thumbnail = await db.get_thumbnail(user_id)
-    
-    await query.message.edit_text(
+      # Assuming you have a method in your database to get the user's thumbnail
+      user_thumbnail = await db.get_thumbnail(user_id)
+      
+      await query.message.edit_text(
         text=Txt.THUMB_TXT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([[
