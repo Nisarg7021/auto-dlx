b6f5e5452b4d8ff6cc7bb25c4a7494dbc1e2988c
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-03 11:25:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/c9e6c33aad7f8526f3118.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6446763201').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002020833784") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002099814452"))
 
     # wes response configuration     
