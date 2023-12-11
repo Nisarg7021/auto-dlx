26bdce2d33cac92b622eda435eac3f74dab55169
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-11 19:12:26 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -13,33 +13,26 @@ import os
 import time
 import re
 
+import re
+
 def extract_episode_number(filename):
-    # Pattern 1: S1E01 or S01E01 Extraction
-    pattern1 = re.compile(r'S(\d+)\s*[E|EP]\s*(\d+)', re.IGNORECASE)
+    # Pattern 1: S1E1 or S01E1 Extraction
+    pattern1 = re.compile(r'S(\d{0,2})\s*[E|EP](\d{1,2})', re.IGNORECASE)
 
-    # Pattern 2: S02 E01 Extraction
-    pattern2 = re.compile(r'S(\d+)\s*-\s*[E|EP]\s*(\d+)', re.IGNORECASE)
+    # Pattern 2: S02 E1 Extraction
+    pattern2 = re.compile(r'S(\d{0,2})\s*-\s*[E|EP](\d{1,2})', re.IGNORECASE)
 
-    # Pattern 3: Steins Gate 0 - S2 E17 [Dual] 2160p @Anime_Fair.mkv
-    pattern3 = re.compile(r'S\d+\s*[E|EP]\s*(\d+)', re.IGNORECASE)
+    # Pattern 3: Episode Number After "E" or "EP" or "-"
+    pattern3 = re.compile(r'[E|EP]\s*(\d{1,2})|-(\d{1,2})', re.IGNORECASE)
 
-    # Pattern 4: Episode Number After "E" or "EP" with words
-    pattern4 = re.compile(r'[E|EP]\s*(\d+)', re.IGNORECASE)
-
-    # Pattern 5: Episode Number After "-"
-    pattern5 = re.compile(r'-\s*(\d+)', re.IGNORECASE)
-
-    # Pattern 6: S02 - EP20 Jujutsu Kaisen [480p] [Sub] @Animes_Xyz.mkv
-    pattern6 = re.compile(r'S(\d+)\s*-\s*[E|EP]\s*(\d+)', re.IGNORECASE)
-
-    # Pattern 7: Standalone Episode Number
-    pattern7 = re.compile(r'(\d+)', re.IGNORECASE)
+    # Pattern 4: Standalone Episode Number
+    pattern4 = re.compile(r'(\d{1,2})', re.IGNORECASE)
 
     # Try each pattern in order
-    for pattern in [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7]:
+    for pattern in [pattern1, pattern2, pattern3, pattern4]:
         match = re.search(pattern, filename)
         if match:
-            episode_number = match.group(1)  # Extracted episode number
+            episode_number = match.group(1) or match.group(2)  # Extracted episode number
             return episode_number
 
     # Return None if no pattern matches
@@ -47,15 +40,13 @@ def extract_episode_number(filename):
 
 # Test the function with examples
 filenames = [
-    "Steins Gate 0 - S2 E17 [Dual] 2160p @Anime_Fair.mkv",
-    "S02 - EP20 Jujutsu Kaisen [480p] [Sub] @Animes_Xyz.mkv",
-    "S02 - EP19 Jujutsu Kaisen [1080p] [Sub] @Animes_Xyz.mkv",
-    "Another Example S1E05.mkv",
-    "One Piece S1-07 [720p][Dual] @Anime_Edge.mkv",
-    "One Piece 2000 @Anime_Edge.mkv",
-    "EP03 Example Anime.mkv",
-    "Anime - EP15 - Title.mkv",
-    "Anime Title - 05 - Subs.mkv"
+    "S1E1",
+    "S01E1",
+    "S02 E1",
+    "S02 E01",
+    "One Piece S1-7",
+    "One Piece S1-07",
+    "One Piece 2000",
 ]
 
 for filename in filenames:
