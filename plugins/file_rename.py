aa42185cf5c88d77626c836a427c8b438f6be91d
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2024-01-11 16:52:33 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -1,3 +1,5 @@
+# Made by @Nation_Bots
+
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import InputMediaDocument, Message 
@@ -137,34 +139,12 @@ def extract_episode_number(filename):
     return None
 
 # Example Usage:
-filename = "One Piece S1-07 [720p][Dual] @Anime_Edge.mkv"
+filename = "One Piece S1-07 [720p][Dual] @Animes_Xyz.mkv"
 episode_number = extract_episode_number(filename)
 print(f"Extracted Episode Number: {episode_number}")
 
-@Client.on_message(filters.private & filters.command("autorename"))
-async def auto_rename_command(client, message):
-    user_id = message.from_user.id
-
-    # Extract the format from the command
-    format_template = message.text.split("/autorename", 1)[1].strip()
-
-    # Save the format template to the database
-    await db.set_format_template(user_id, format_template)
-
-    await message.reply_text("Auto rename format updated successfully!")
-
-@Client.on_message(filters.private & filters.command("setmedia"))
-async def set_media_command(client, message):
-    user_id = message.from_user.id    
-    media_type = message.text.split("/setmedia", 1)[1].strip().lower()
-
-    # Save the preferred media type to the database
-    await db.set_media_preference(user_id, media_type)
-
-    await message.reply_text(f"Media preference set to: {media_type}")
-
 # Inside the handler for file uploads
-@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio)) & filters.user(Config.ADMIN))
 async def auto_rename_files(client, message):
     user_id = message.from_user.id
     firstname = message.from_user.first_name
